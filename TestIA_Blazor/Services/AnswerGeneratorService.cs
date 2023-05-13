96fa0ea1d94b0f42d0ca36d83a85b4250c12c554
Juan Fernando Gonzalez Lopez <juan2036@hotmail.com>
2023-05-13 11:21:27 -0500

    Agregar archivos de proyecto.

---
+++ TestIA_Blazor/Services/AnswerGeneratorService.cs
@@ -0,0 +1,31 @@
+﻿using OpenAI_API;
+using OpenAI_API.Completions;
+
+namespace TestIA_Blazor.Services
+{
+    public class AnswerGeneratorService: IAnswerGeneratorService
+    {
+        public async Task<string> GenerateAnswer(string Prompt)
+        {
+            string apiKey = "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";
+            string answer = string.Empty;
+
+            var openai = new OpenAIAPI(apiKey);
+            CompletionRequest completion = new CompletionRequest();
+            completion.Prompt = Prompt;
+            completion.MaxTokens= 4000;
+            var result = await openai.Completions.CreateCompletionAsync(completion);
+
+            if(result != null)
+            {
+                foreach(var item in result.Completions)
+                {
+                    answer = item.Text;
+                }
+            }
+
+            return answer;
+        }
+
+    }
+}
---
+++ TestIA_Blazor/Services/IAnswerGeneratorService.cs
@@ -0,0 +1,8 @@
+﻿namespace TestIA_Blazor.Services
+{
+    public interface IAnswerGeneratorService
+    {
+        Task<string> GenerateAnswer(string answer);
+
+    }
+}
